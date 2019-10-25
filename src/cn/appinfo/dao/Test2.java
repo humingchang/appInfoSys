749fb76af6e7181951c4fc7c81b7e842ea556d32
humingchang <2353994425@qq.com>
2019-10-25 11:04:30 +0800

    修改第二次test2

--- src/cn/appinfo/dao/Test2.java
+++ src/cn/appinfo/dao/Test2.java
@@ -2,7 +2,7 @@ package cn.appinfo.dao;
 
 public class Test2 {
 	public static void main(String[] args) {
-		
+		System.out.println("ÄãºÃ£¡");
 	}
 private void syso() {
 	// TODO Auto-generated method stub
